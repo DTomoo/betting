e291ea49f104b905196051ea7b97b499d2883eef
Danyi <tamas.danyi@gmail.com>
2018-05-13 14:03:18 +0200

    added TeamDataRepository

---
+++ src/main/java/com/dt/betting/db/repository/inmemory/TeamDataRepository.java
@@ -0,0 +1,9 @@
+package com.dt.betting.db.repository.inmemory;
+
+import org.springframework.stereotype.Component;
+
+import com.dt.betting.db.domain.Team;
+
+@Component
+class TeamDataRepository extends InMemoryDataRepository<Team> {
+}
